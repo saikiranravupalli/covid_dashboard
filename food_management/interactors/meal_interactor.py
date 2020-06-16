6431266dc6c4dbacd1b9973b77113876cbcfa0e9
saikiran <saikiranravupalli@gmail.com>
2020-06-16 04:45:24 +0000

    change validate items quantity in Meal Interactor

--- food_management/interactors/meal_interactor.py
+++ food_management/interactors/meal_interactor.py
@@ -79,7 +79,10 @@ class MealInteractor:
     @staticmethod
     def _validate_items_quantity(item_dtos):
         invalid_items_quantity = [
-            {item_dto.item_id: item_dto.quantity}
+            {
+                "item_id": item_dto.item_id,
+                "item_quantity":item_dto.quantity
+            }
             for item_dto in item_dtos
             if item_dto.quantity < 0
         ]
