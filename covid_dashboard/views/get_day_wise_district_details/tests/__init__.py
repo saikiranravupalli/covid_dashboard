520edfb7ea3740fb79eeaa7e483d28d02eb9939a
saikiran <saikiranravupalli@gmail.com>
2020-06-30 04:08:32 +0000

    update feature dev

--- covid_dashboard/views/get_day_wise_district_details/tests/__init__.py
+++ covid_dashboard/views/get_day_wise_district_details/tests/__init__.py
@@ -6,7 +6,9 @@ REQUEST_METHOD = "get"
 URL_SUFFIX = "districts/{district_id}/day_wise/details/v1/"
 
 from .test_case_01 import TestCase01GetDayWiseDistrictDetailsAPITestCase
+from .test_case_02 import TestCase02GetDayWiseDistrictDetailsAPITestCase
 
 __all__ = [
-    "TestCase01GetDayWiseDistrictDetailsAPITestCase"
+    "TestCase01GetDayWiseDistrictDetailsAPITestCase",
+    "TestCase02GetDayWiseDistrictDetailsAPITestCase"
 ]
